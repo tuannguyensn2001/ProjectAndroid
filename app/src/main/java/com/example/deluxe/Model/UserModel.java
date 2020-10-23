76765e79b2fa7c74cc30a83ac07122d9f7c9c9b8
unknown <vinhden687@gmail.com>
2020-10-23 11:39:59 +0700

    viet chuc nang userModel

---
+++ app/src/main/java/com/example/deluxe/Model/UserModel.java
@@ -0,0 +1,33 @@
+package com.example.deluxe.Model;
+
+import com.example.deluxe.Entity.User;
+import java.util.ArrayList;
+
+public class UserModel {
+    ArrayList<User> listUser;
+
+    public UserModel(){
+        init();
+    }
+    public boolean checkUserExist(User user){
+
+        for(User i : listUser){
+            if(user.getUser() == i.getUser() && user.getPassword()==i.getPassword()){
+                return  true;
+            }
+        }
+        return false;
+    }
+
+    public void init(){
+        listUser = new ArrayList<User>();
+        listUser.add(new User("nguyenvana","123456") );
+        listUser.add(new User("nguyenvanb","121212"));
+        listUser.add(new User("phamthiduc" , "123123"));
+        listUser.add(new User("tranmaihuong","321321"));
+        listUser.add(new User("nguyenngocchau","111111"));
+        listUser.add(new User("dangtrungdung","222222"));
+        listUser.add(new User("nguyenvantuan","123456"));
+
+    }
+}
