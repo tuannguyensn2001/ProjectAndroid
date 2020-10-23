4487ac3951ba034b554f63a414598fdf17cc0295
Hươngg Hươngg <huongtran062001@gmail.com>
2020-10-23 11:43:30 +0700

    Chuc nang rules

---
+++ app/src/main/java/com/example/deluxe/helper/Rules.java
@@ -0,0 +1,21 @@
+package com.example.deluxe.helper;
+
+public class Rules {
+    public static boolean required(String s){
+        if(s.length()>0) return true;
+        return false;
+    }
+   public static boolean min(String value,int length){
+        if(value.length()>=length) return true;
+        return false;
+   }
+   public static boolean isEmail(String value){
+       String emailPattern = "\\w+@\\w+[.]\\w+";
+       boolean flag = value.matches(emailPattern);
+       if(!flag) return false;
+       return true;
+   }
+    
+
+    
+}
