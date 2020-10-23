8618883dad918f0d2eb81e0dca09eabc57a4fac7
tdungrv1901 <tdungrvk1901@gmail.com>
2020-10-23 11:49:15 +0700

    giao dien Login

--- app/src/main/java/com/example/deluxe/View/MainActivity.java
+++ app/src/main/java/com/example/deluxe/View/MainActivity.java
@@ -14,38 +14,12 @@ import com.example.deluxe.R;
 public class MainActivity extends AppCompatActivity implements LoginInterface.LoginView {
     private Button submitButton;
     private LoginInterface.LoginPresenter Login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        init();
-        this.submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handleClickButton();
-            }
-        });
-    }
-
-
-    public void init()
-    {
-        this.submitButton = (Button) findViewById(R.id.button);
-        this.Login = new Login(this);
-    }
-
-    @Override
-    public void handleClickButton() {
-        String username = "A";
-        String password = "B";
-        Login.handleLogin(username,password);
-    }
-
-    @Override
-    public void setError()
-    {
-        Toast.makeText(MainActivity.this, "Có lỗi", Toast.LENGTH_SHORT).show();
     }
 }
+
