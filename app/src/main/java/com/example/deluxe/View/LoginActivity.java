1579dda949a57f09990378a930463168ed942dce
tdungrv1901 <tdungrvk1901@gmail.com>
2020-10-27 16:57:42 +0700

    Design Newpass ForgotPass and VerifyPass

--- app/src/main/java/com/example/deluxe/View/LoginActivity.java
+++ app/src/main/java/com/example/deluxe/View/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.deluxe.View;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -45,7 +44,7 @@ public class LoginActivity extends AppCompatActivity implements LoginInterface.L
         this.submitButton = (Button) findViewById(R.id.submitButton);
         this.Login = new LoginPresenter(this);
         this.username = (EditText) findViewById(R.id.usernameInput);
-        this.password = (EditText) findViewById(R.id.passwordInput);
+        this.password = (EditText) findViewById(R.id.emailInput);
 
     }
 
