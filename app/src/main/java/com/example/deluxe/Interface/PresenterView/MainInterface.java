3e2e60678666a9249e4bbe69febfbc3154baf0b9
unknown <devpro2001@gmail.com>
2020-10-29 00:15:41 +0700

    demo

---
+++ app/src/main/java/com/example/deluxe/Interface/PresenterView/MainInterface.java
@@ -0,0 +1,16 @@
+package com.example.deluxe.Interface.PresenterView;
+
+import com.example.deluxe.Core.View;
+
+public interface MainInterface {
+    public interface MainPresenter
+    {
+        void handleLogOut();
+
+    }
+
+    public interface MainView extends View
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/deluxe/Presenter/MainPresenter.java
@@ -0,0 +1,21 @@
+package com.example.deluxe.Presenter;
+
+import com.example.deluxe.Interface.PresenterView.MainInterface;
+import com.example.deluxe.Model.Auth;
+import com.example.deluxe.View.LoginActivity;
+
+public class MainPresenter implements MainInterface.MainPresenter {
+
+    private MainInterface.MainView mainView;
+
+    public MainPresenter(MainInterface.MainView view)
+    {
+        this.mainView = view;
+    }
+
+    @Override
+    public void handleLogOut() {
+        Auth.getInstance().logout();
+        mainView.loadView(LoginActivity.class);
+    }
+}
