40b7b0d4b4846eb0aa6c6cff89b7ea51438572e6
unknown <devpro2001@gmail.com>
2020-11-03 17:09:45 +0700

    setup Search User

---
+++ app/src/main/java/com/example/deluxe/Presenter/SearchViewPresenter.java
@@ -0,0 +1,10 @@
+package com.example.deluxe.Presenter;
+
+import com.example.deluxe.Interface.PresenterView.SearchUser;
+
+public class SearchViewPresenter implements SearchUser.SearchUserPresenter {
+    @Override
+    public void handleInput(String emailSearch) {
+
+    }
+}
