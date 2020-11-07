34a8171a524310ceb31f680cdab8144d90a67d49
unknown <devpro2001@gmail.com>
2020-11-07 09:21:49 +0700

    .

--- app/src/main/java/com/example/deluxe/Presenter/SearchUserPresenter.java
+++ app/src/main/java/com/example/deluxe/Presenter/SearchUserPresenter.java
@@ -1,7 +1,5 @@
 package com.example.deluxe.Presenter;
 
-import android.util.Log;
-
 import com.example.deluxe.Entity.User;
 import com.example.deluxe.Interface.Model.UserInterface;
 import com.example.deluxe.Interface.PresenterView.SearchUserInterface;
@@ -11,28 +9,32 @@ import com.example.deluxe.Model.UserModel;
 import java.util.ArrayList;
 
 public class SearchUserPresenter implements SearchUserInterface.SearchUserPresenter {
-	UserModel userModel;
-	SearchUserInterface.SearchUserView searchUserView;
+    UserModel userModel;
+    SearchUserInterface.SearchUserView searchUserView;
 
-	public SearchUserPresenter(SearchUserInterface.SearchUserView searchUserView) {
-		this.searchUserView = searchUserView;
-		this.userModel = new UserModel();
-	}
+    public SearchUserPresenter(SearchUserInterface.SearchUserView searchUserView) {
+        this.searchUserView = searchUserView;
+        this.userModel = new UserModel();
+    }
 
-	@Override
-	public void handleInput(String emailSearch) {
-		final User user = new User(null, null, emailSearch);
-		this.userModel.search(user, new UserInterface() {
-			@Override
-			public void dataIsLoaded(ArrayList<User> list) {
-				if (!list.isEmpty())
-					for (User userInList : list) {
-						if (userInList.getEmail().equals(Auth.getInstance().user().getEmail()))
-							list.remove(userInList);
-					}
+    @Override
+    public void handleInput(String emailSearch) {
+        final User user = new User(null, null, emailSearch);
+        this.userModel.search(user, new UserInterface() {
+            @Override
+            public void dataIsLoaded(ArrayList<User> list) {
 
-				searchUserView.setList(list);
-			}
-		});
-	}
+                if (!list.isEmpty())
+                    for (User userInList : list) {
+                        if (userInList.getEmail().equals(Auth.getInstance().user().getEmail())) {
+                            list.remove(userInList);
+                            break;
+                        }
+
+                    }
+
+                searchUserView.setList(list);
+            }
+        });
+    }
 }
